e2e73d86123c90a615cf7b0fa44e0a0770f129df
凌 <a111@123.local>
2016-04-26 12:51:58 +0800

    Initial Commit

---
+++ EmojiRain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmojiRain
+//
+//  Created by xuanfeng on 16/4/26.
+//  Copyright © 2016年 LXF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EmojiRain/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmojiRain
+//
+//  Created by xuanfeng on 16/4/26.
+//  Copyright © 2016年 LXF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
